56a528515b42d5e781eac679cf558ede8dae312d
Mohamed Adel <mohamedadelzmd@gmail.com>
2024-08-07 15:02:50 +0300

    remove placeholder projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,30 +28,6 @@ export default function Projects({ refProps }) {
           <li>Receiver : Filtering and Reshaping signal.</li>
         </ul>
       </Project>
-      <Project
-        name={"Wireless controlled Light"}
-        image={"./images/project1.jpg"}
-        imageOnRight={false}
-        key={3}
-      >
-        Working with team we achieved the full mark facing the deadline in just
-        2 days, The project is about controling an RGB led using a mobile
-        application, including the light intensity and the degree of the color.
-      </Project>
-      <Project
-        name={"Digital Filter and Sound editing"}
-        image={"./images/project2.jpg"}
-        key={4}
-      >
-        Used MATLAB to design a full system:
-        <br />
-        <br />
-        <ul>
-          <li>Transmitter : sending sound.</li>
-          <li>Channel : affecting signal and adding random noise.</li>
-          <li>Receiver : Filtering and Reshaping signal.</li>
-        </ul>
-      </Project>
     </section>
   );
 }
